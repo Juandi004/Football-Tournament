3bab506d39a489542750c3c1e61f9cc9a6363cf4
Juan Gutierrez <juandiender123@gmail.com>
2026-08-18 15:35:15 -0500

    update match-dto

--- football-api/src/match/dto/create-match.dto.ts
+++ football-api/src/match/dto/create-match.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { 
   IsDateString, 
   IsEnum, 
@@ -10,28 +11,34 @@ import {
 import { Status } from 'generated/prisma/enums';
 
 export class CreateMatchDto {
+  @ApiProperty()
   @IsNotEmpty({ message: 'El equipo local es obligatorio' })
   @IsUUID('4', { message: 'El ID del equipo local debe ser un UUID válido' })
   homeTeamId!: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'El equipo visitante es obligatorio' })
   @IsUUID('4', { message: 'El ID del equipo visitante debe ser un UUID válido' })
   awayTeamId!: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsDateString({}, { message: 'La fecha debe tener un formato ISO válido' })
   matchDate?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsInt()
   @Min(0)
   homeScore?: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsInt()
   @Min(0)
   awayScore?: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsEnum(Status, { message: 'El estado no es válido' })
   status?: Status;
