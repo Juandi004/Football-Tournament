efd29d52e57071b5fbff1f3b57fa1de4f87dd414
Juan Gutierrez <juandiender123@gmail.com>
2026-08-19 15:33:05 -0500

    add role decorator

--- football-api/src/auth/dto/create-auth.dto.ts
+++ football-api/src/auth/dto/create-auth.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsStrongPassword, MinLength } from "class-validator";
 
 export class CreateAuthDto {
+    @ApiProperty()
     @IsEmail()
     email!: string;
 
+    @ApiProperty()
     @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
     password!: string
 }
